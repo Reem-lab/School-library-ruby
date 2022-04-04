6ab9afdf85570a029a986492ceb44e015045963c
Reem <janinareem@gmail.com>
2022-04-05 01:35:50 +0200

    Update the person file and remove public keyword and add dynamic id

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 class Person
-  def initialize(id, age, name = 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -9,18 +9,16 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
+  def can_use_services?
+    return true if @parent_permission == true || of_age?
+
+    false
+  end
+  
   private
 
   def of_age?
     return true if @age.to_i >= 18
-
-    false
-  end
-
-  public
-
-  def can_use_services?
-    return true if @parent_permission == true && of_age?
 
     false
   end
