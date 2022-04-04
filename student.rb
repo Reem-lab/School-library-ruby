22581219cb4dd62ec6298070df46d6cf1f46ad80
Reem <janinareem@gmail.com>
2022-04-04 21:03:36 +0200

    Add class student inherited from person

---
+++ student.rb
@@ -0,0 +1,15 @@
+require "./person.rb"
+
+class Student < Person
+  def initialize(id, name, age, parent_permission, classroom)
+    super(id, name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+student1 = Student.new(2, "nor", 23, true, 7)
+puts student1.play_hooky
