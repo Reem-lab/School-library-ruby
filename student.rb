5fe412b2ddd4a63299a8a741b86ce6e8cdfa7cb8
Reem <janinareem@gmail.com>
2022-04-06 22:54:56 +0200

    Add relationship belonges to student and clasroom

--- student.rb
+++ student.rb
@@ -1,9 +1,17 @@
 require_relative './person'
+require_relative './classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classrooms
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 
   def play_hooky
