868c04f87817972a22579c4726c79935c68e89ae
Reem <janinareem@gmail.com>
2022-04-06 22:55:53 +0200

    Add relationship in the person and rentals and book

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './decorateor'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
