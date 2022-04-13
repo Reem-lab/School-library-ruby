8fb72254f2dd3927d9b0337f66f68c5d15fdcfd1
Reem <janinareem@gmail.com>
2022-04-13 05:08:15 +0200

    Add person to teacher class

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name, parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, specialization, name, parent_permission: true, id: Random.rand(1..1000))
+    super(age, name, parent_permission: parent_permission, id: id)
     @specialization = specialization
   end
 
