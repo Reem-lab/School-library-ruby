b856f6f2a605d1980372d2d9a58339275502cef7
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-13 15:37:01 -0600

    ADD TESTS FOR Teacher CLASS

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require_relative '../book'
 require_relative '../person'
 require_relative '../student'
+require_relative '../teacher'
 require_relative '../classroom'
 
 require 'yaml'
---
+++ spec/teacher_spec.rb
@@ -0,0 +1,43 @@
+require_relative './spec_helper'
+
+describe Teacher do
+  before :each do
+    @teacher = Teacher.new 50, 'politics', 'name', parent_permission: true, id: 154
+  end
+
+  describe '#new' do
+    it 'takes four parameters and returns a Teacher object' do
+      @teacher.should be_an_instance_of Teacher
+    end
+  end
+
+  describe '#age' do
+    it 'returns the correct age' do
+      @teacher.age.should eql 50
+    end
+  end
+
+  describe '#specialization' do
+    it 'returns the correct specialization' do
+      @teacher.specialization.should eql 'politics'
+    end
+  end
+
+  describe '#name' do
+    it 'returns the correct name' do
+      @teacher.name.should eql 'name'
+    end
+  end
+
+  describe '#parent_permission' do
+    it 'returns the correct parent_permission ' do
+      @teacher.parent_permission.should eql true
+    end
+  end
+
+  describe '#id' do
+    it 'returns the correct id' do
+      @teacher.id.should eql 154
+    end
+  end
+end
