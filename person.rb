3c003cea5d1efa521ae0cc94e146c9be46206f3b
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-12 19:03:37 -0600

    ADD MISSING attr_accessor :parent_permission

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative './decorateor'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :rentals, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
