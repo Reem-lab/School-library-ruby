4ae2b7326f1a861e21eda54e088b6d163a979264
Reem <janinareem@gmail.com>
2022-04-13 05:07:57 +0200

    Update id in personm

--- person.rb
+++ person.rb
@@ -4,9 +4,9 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :rentals, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, id: Random.rand(1..1000))
     super()
-    @id = Random.rand(1..1000)
+    @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
