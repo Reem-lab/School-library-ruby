7ec4ef1138b050db2ba178003ae19e401f278767
Reem <janinareem@gmail.com>
2022-04-13 03:21:42 +0200

    Add function to write books and rentals

--- app.rb
+++ app.rb
@@ -126,15 +126,31 @@ class App
 
   def write_json
     persons = @person.each_with_index.map do |person, index|
-      { 'class' => person.class, 'age' => person.age, 'name' => person.name,
-        'specialization' => (person.specialization if person.instance_of?(Teacher)),
-        'parent_permission' => person.parent_permission, 'index' => index }
+      { 'class': person.class, 'age': person.age, 'name': person.name,
+        'specialization': (person.specialization if person.instance_of?(Teacher)),
+        'parent_permission': person.parent_permission, 'index': index }
     end
 
-    puts persons
+    json_person = JSON.generate(persons)
+    File.write('person.json', json_person)
 
-    json = JSON.generate(persons)
-    File.write('person.json', json)
+    books = @books.each_with_index.map do |book, index|
+      {
+        'title': book.title, 'author': book.author, 'index': index
+      } 
+    end
+    json_books = JSON.generate(books)
+    File.write('books.json', json_books)
+
+    rentals = @rentals.each_with_index.map do |rental, index|
+      {
+        'date': rental.date, 'book_index': @books.index(rental.book),
+         'person_index': @person.index(rental.person)
+      }
+    end
+
+    json_rentals = JSON.generate(rentals)
+    File.write('rentals.json', json_rentals)
   end
 
   # def load_person
