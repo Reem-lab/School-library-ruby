e0bead727f178217c3d11b39724ec82a180270cb
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-12 19:03:50 -0600

    ADD MISSING attr_accessor :specialization

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(age, specialization, name, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
