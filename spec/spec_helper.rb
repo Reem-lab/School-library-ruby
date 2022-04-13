6fa1299265971c1b9a3cc07cb0ab0696ea521edb
Reem <janinareem@gmail.com>
2022-04-14 00:16:38 +0200

    Add the path to rentals and decorator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,7 @@ require_relative '../person'
 require_relative '../student'
 require_relative '../teacher'
 require_relative '../classroom'
+require_relative '../rental'
+require_relative '../decorateor'
 
 require 'yaml'
